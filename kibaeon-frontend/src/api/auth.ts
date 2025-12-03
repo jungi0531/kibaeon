7fa844188f34575321ec69c0c87631f89c6cee67
jungi Hong <hjg020531@naver.com>
2025-12-03 17:02:01 +0900

    auth.ts 파일 삭제

--- kibaeon-frontend/src/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-export const checkEmailDuplicated = async (email: string) => {
-    const res = await api.get("/check-email", {
-        params: { email }
-    });
-
-    return res.data as boolean;
-}
