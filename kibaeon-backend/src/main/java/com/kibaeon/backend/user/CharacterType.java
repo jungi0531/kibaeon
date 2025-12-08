ab356a192c62d9b90037dfd3cd23815aea8d4ea7
jungi Hong <hjg020531@naver.com>
2025-12-08 18:52:02 +0900

    refactor: CharacterType 라벨 이름 통일

--- kibaeon-backend/src/main/java/com/kibaeon/backend/user/CharacterType.java
+++ kibaeon-backend/src/main/java/com/kibaeon/backend/user/CharacterType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CharacterType {
     KEYCAP_01("ㅁ 키캡", "KEYCAP_01.png"),
-    KEYCAP_02("ㄴ 키캡 기본", "KEYCAP_02.png"),
-    KEYCAP_03("ㅇ 키캡 기본", "KEYCAP_03.png");
+    KEYCAP_02("ㄴ 키캡", "KEYCAP_02.png"),
+    KEYCAP_03("ㅇ 키캡", "KEYCAP_03.png");
 
     private final String name;
     private final String image;
